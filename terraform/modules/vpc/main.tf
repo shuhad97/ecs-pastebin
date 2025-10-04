f448d7adfca155907e5fded2425b478d5e585d59
shuhad97 <shuhadc@gmail.com>
2025-10-04 21:33:06 +0100

    Networking foundations

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,59 @@
+resource "aws_vpc" "vpc" {
+    cidr_block = var.vpc_cidr
+
+    tags = {
+      Name = "${var.project_name}-vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = var.public_subnet_cidr
+    
+    tags = {
+      Name = "${var.project_name}-public-subnet"
+    }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = var.private_subnet_cidr_1
+    availability_zone = var.private_subnet_1_az
+    
+    tags = {
+      Name = "${var.project_name}-private-subnet-1"
+    }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = var.private_subnet_cidr_2
+    availability_zone = var.private_subnet_2_az
+    
+    tags = {
+      Name = "${var.project_name}-private-subnet-2"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+
+    tags = {
+        Name = "${var.project_name}-igw"
+    }
+}
+
+resource "aws_route_table" "public_route_table"{
+    vpc_id = aws_vpc.vpc.id
+
+    route{
+        cidr_block = var.public_route_table_cidr
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+}
+
+resource "aws_route_table_association" "public_assoc" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public_route_table.id
+}
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,46 @@
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/22"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.0.0/25"
+}
+
+variable "private_subnet_cidr_1" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.0.128/24"
+}
+
+variable "private_subnet_cidr_2" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_1_az" {
+  description = "Availability zone for subnet 1"
+  type        = string
+  default = "eu-west-2a"
+}
+
+variable "private_subnet_2_az" {
+  description = "Availability zone for subnet 2"
+  type        = string
+  default = "eu-west-2b"
+}
+
+variable "public_route_table_cidr" {
+   description = "Route table CIDR"
+   type        = string
+}
+
+variable "project_name" {
+  type        = string
+  default     = "ecs-pastebin"
+}
