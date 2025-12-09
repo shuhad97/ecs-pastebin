1504d63061b2cbe316fdca7ba937f8c0e8ff05e5
shuhad97 <shuhadc@gmail.com>
2025-12-09 17:37:17 +0000

    Add in bucket polocy for ALB access

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -3,3 +3,26 @@ resource "aws_s3_bucket" "logs_bucket" {
   bucket = "pastebin-logs"
 
 }
+
+resource "aws_s3_bucket_policy" "alb_access" {
+  bucket = aws_s3_bucket.logs_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSALBAccessLogsPolicy"
+        Effect = "Allow"
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        }
+        Action = [
+          "s3:PutObject",
+          "s3:GetBucketLocation"
+        ]
+        Resource = [
+          "arn:aws:s3:::pastebin-logs",
+        ]
+      }
+    ]
+  })
+}
