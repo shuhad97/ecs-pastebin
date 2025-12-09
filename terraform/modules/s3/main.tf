6d5744f0c900d75e6192ffb87d63ced79aa99971
shuhad97 <shuhadc@gmail.com>
2025-12-09 17:52:50 +0000

    Add in bucket correct resource naming

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "logs_bucket" {
 
 resource "aws_s3_bucket_policy" "alb_access" {
   bucket = aws_s3_bucket.logs_bucket.id
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -20,7 +21,8 @@ resource "aws_s3_bucket_policy" "alb_access" {
           "s3:GetBucketLocation"
         ]
         Resource = [
-          "arn:aws:s3:::pastebin-logs",
+          "${aws_s3_bucket.logs_bucket.arn}",
+          "${aws_s3_bucket.logs_bucket.arn}/*"
         ]
       }
     ]
